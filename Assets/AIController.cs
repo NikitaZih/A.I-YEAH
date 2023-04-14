962f3367179d13085dc1188a8ad7c41e5d7df0e6
HomelessPiano <homelesspiano@gmail.com>
2023-04-14 10:35:03 +0100

    code

--- Assets/AIController.cs
+++ Assets/AIController.cs
@@ -1,13 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class AIController : MonoBehaviour
 {
+    private NavMeshAgent _navMeshAgent;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _navMeshAgent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
