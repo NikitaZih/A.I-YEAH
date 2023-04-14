1c3ae08715e8953b12ef5ff7fe2dd94ea8f2c26b
HomelessPiano <homelesspiano@gmail.com>
2023-04-14 20:10:26 +0100

    CODE STILL NOT WORKING

--- Assets/Scripts/AIController.cs
+++ Assets/Scripts/AIController.cs
@@ -15,6 +15,18 @@ public class AIController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetMouseButtonDown(0))
+                {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out RaycastHit hit))
+            {
+                SetAITargetLocation(hit.point);
+            }
+        }
+    }
+
+private void SetAITargetLocation(Vector3 targetLocation)
+    {
+        -_navMeshAgent.SetDestination(targetLocation);
     }
 }
