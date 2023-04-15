a5693ffd6c6c4a7bb0d47e0deee7a99a8d3d4a7b
HomelessPiano <homelesspiano@gmail.com>
2023-04-15 13:10:21 +0100

    anxious ai

---
+++ Assets/Scripts/AnxiousAI.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AnxiousAI : MonoBehaviour
+{
+    private NavMeshAgent _agent;
+
+    public GameObject Player;
+
+    public float EnemyDistanceRun = 4.0f;
+
+    void Start()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+    }
+
+
+    void Update()
+    {
+        float distance = Vector3.Distance(transform.position, Player.transform.position);
+        Debug.Log("Distance:" + distance);
+
+
+
+        if (distance < EnemyDistanceRun)
+        {
+            Vector3 dirToPlayer = transform.position - Player.transform.position;
+
+            Vector3 newPos = transform.position + dirToPlayer;
+            _agent.SetDestination(newPos);
+        }
+    }
+}
